7d58cc8e7597fba694569593fcf9ec519d12168a
MarioBatalha <mariobatalha9@gmail.com>
2023-12-12 02:24:53 +0100

    :sparkles: feat: hero section

--- views/institutional/components/hero/index.tsx
+++ views/institutional/components/hero/index.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@interest-protocol/ui-kit';
+import { Box, Button, Typography } from '@interest-protocol/ui-kit';
 import { FC } from 'react';
 
 const Hero: FC = () => (
@@ -9,7 +9,50 @@ const Hero: FC = () => (
     backgroundPosition="bottom center"
     backgroundSize="cover"
   >
-    <Box mt="2xl">Hero</Box>
+    <Box
+      height="100vh"
+      width="100vw"
+      overflow="hidden"
+      bg="url(/img/hero-illustration.svg)"
+      backgroundPosition="bottom center"
+      backgroundSize="cover"
+    >
+      <Box
+        width="100vw"
+        height="100vh"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          width={['100%', '100%', '100%', '40%']}
+          height={['100%', '100%', '100%', '35%']}
+        >
+          <Typography
+            textAlign="center"
+            as="h1"
+            whiteSpace="wrap"
+            fontSize={['2.5rem', '3.563rem']}
+            fontWeight="900"
+            lineHeight="4rem"
+            textTransform="uppercase"
+            color="onSurface"
+            size="large"
+            variant="body"
+          >
+            Streamlined Development for Move Creators
+          </Typography>
+          <Button textTransform="uppercase" my="4rem" variant="filled">
+            Get started
+          </Button>
+        </Box>
+      </Box>
+    </Box>
   </Box>
 );
 
